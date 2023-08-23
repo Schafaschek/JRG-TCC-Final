806c9bb312253b5a42735517c1e544c605f1021b
Bruno Schafachek <schafaschekfleisch@gmail.com>
2023-08-23 17:01:36 -0300

    Versão final TCC

---
+++ MainClass.java
@@ -0,0 +1,21 @@
+package br.edu.ifsc.javarg;
+
+import br.edu.ifsc.javargexamples.A;
+import br.edu.ifsc.javargexamples.B;
+import br.edu.ifsc.javargexamples.C;
+import br.edu.ifsc.javargexamples.Aextend;
+import br.edu.ifsc.javargexamples.AextendExtend;
+
+@SuppressWarnings("all")
+public class MainClass {
+
+    public static void main(String[] args) {
+        long pq = -5;
+        if (true || false) {
+            br.edu.ifsc.javargexamples.Aextend la = new br.edu.ifsc.javargexamples.Aextend(4688, -2111222950);
+        } else {
+            short yc = -11;
+        }
+        br.edu.ifsc.javargexamples.C rs = new br.edu.ifsc.javargexamples.C();
+    }
+}
